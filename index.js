092424b43698a8fb83e7d1c8be1587b33d510f39
Ernesto <48799528+ernestod10@users.noreply.github.com>
2021-01-12 22:59:12 -0400

    aaaa

--- index.js
+++ index.js
@@ -14,9 +14,24 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
+//Crud Productos
 app.get('/Productos', db.getProductos)
 app.get('/Productos/:id', db.geProductoId)
 app.post('/newProduct',db.createProducto)
+app.put('/updateProducto/:id', db.updateProducto)
+app.delete('/deleteProducto/:id',db.elimProducto)
+//Crud Paises
+app.get('/Paises', db.getPaises)
+app.get('/Paises/:id', db.getPaisId)
+app.post('/newPais',db.createPais)
+app.put('/updatePais/:id', db.updatePais)
+app.delete('/deletePais/:id',db.elimPais)
+//Crud Importes
+app.get('/Import', db.getPaises)
+app.get('/Import/:id', db.getPaisId)
+app.post('/newImport',db.createPais)
+app.put('/updateImport/:id', db.updatePais)
+app.delete('/deleteImport/:id',db.elimPais)
 
 
 
--- queries.js
+++ queries.js
@@ -1,12 +1,14 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
+  user: 'postgres', //Aqui pongan su user
   host: 'localhost',
-  database: 'Demo_Ambiente',
-  password: '0809',
+  database: 'Demo_Ambiente', //el nombre de su BD
+  password: '0809',           //y la contraseña
   port: 5432,
 })
 //PRODUCTOS
+/* _____________________________________________________________________________________________________________*/
+    //GET todos los productos
     const getProductos = (request, response) => {
         pool.query('SELECT * FROM Producto ORDER BY id_producto ASC', (error, results) => {
         if (error) {
@@ -15,6 +17,7 @@ const pool = new Pool({
         response.status(200).json(results.rows)
         })
     }
+    //GET 1 producto por ID
     const geProductoId = (request, response) => {
         const id = parseInt(request.params.id)
       
@@ -25,19 +28,181 @@ const pool = new Pool({
           response.status(200).json(results.rows)
         })
       }
+    //CREATE 1 producto
     const createProducto = (request, response) => {
-    const { nombre, precio } = request.body
+        const { nombre, Precio_unitario } = request.body
       
-        pool.query('INSERT INTO producto (nombre_producto, Precio_unitario) VALUES ($1, $2)', [nombre, precio], (error, results) => {
+        pool.query('INSERT INTO producto (nombre_producto, Precio_unitario) VALUES ($1, $2)', [nombre, Precio_unitario], (error, results) => {
           if (error) {
             throw error
           }
           response.status(201).send(`Producto añadido con ID: ${result.insertId}`)
         })
       }
+    //UPDATE cambia 1 producto por ID
+    const updateProducto = (request, response) => {
+      const id = parseInt(request.params.id)
+      const { nombre, Precio_unitario } = request.body
+    
+      pool.query(
+        'UPDATE producto SET nombre = $1, Precio_unitario = $2 WHERE id = $3',
+        [nombre, Precio_unitario, id],
+        (error, results) => {
+          if (error) {
+            throw error
+          }
+          response.status(200).send(`Producto modificado ID: ${id}`)
+        }
+      )
+    }
+    //ELIMINA 1 producto con ID
+    const elimProducto = (request, response) => {
+      const id = parseInt(request.params.id)
+    
+      pool.query(' DELETE FROM producto WHERE id_producto = $1', [id], (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Producto eliminado ID: ${id}`)
+      })
+    }
+//PAISES
+/* _____________________________________________________________________________________________________________*/
+//GET todos los Paises
+const getPaises = (request, response) => {
+  pool.query('SELECT * FROM pais ORDER BY id_pais ASC', (error, results) => {
+  if (error) {
+      throw error
+  }
+  response.status(200).json(results.rows)
+  })
+}
+//GET 1 Pais por ID
+const getPaisId = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM pais WHERE id_pais = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+//CREATE 1 producto
+const createPais = (request, response) => {
+  const { nombre } = request.body
+
+  pool.query('INSERT INTO pais (nombre_pais) VALUES ($1)', [nombre], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Pais añadido con ID: ${result.insertId}`)
+  })
+}
+//UPDATE cambia 1 Pais por ID
+const updatePais = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { nombre } = request.body
+
+  pool.query(
+    'UPDATE pais SET nombre = $1 WHERE id = $2',
+    [nombre, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Pais modificado ID: ${id}`)
+    }
+  )
+}
+//ELIMINA 1 Pais con ID
+const elimPais = (request, response) => {
+const id = parseInt(request.params.id)
+
+  pool.query(' DELETE FROM pais WHERE id_pais = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Producto eliminado ID: ${id}`)
+  })
+}
+//IMPORTACIONES
+/* _____________________________________________________________________________________________________________*/
+//GET todas las importaciones
+const getImport = (request, response) => {
+  pool.query('SELECT * FROM importado ORDER BY id_importacion ASC', (error, results) => {
+  if (error) {
+      throw error
+  }
+  response.status(200).json(results.rows)
+  })
+}
+//GET 1 Pais por ID
+const getImportId = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM importado WHERE id_importacion = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+//CREATE 1 producto
+const createImportado = (request, response) => {
+  const { cant,fecha,id_pais,id_producto } = request.body
+
+  pool.query('insert into Importado (cantidad, fecha, pais_id_pais, producto_id_producto) values ($1,$2,$3,$4)', [cant,fecha,id_pais,id_producto], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`importado añadido con ID: ${result.insertId}`)
+  })
+}
+//UPDATE cambia 1 Pais por ID
+const updateImportado = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { cantidad } = request.body
+
+  pool.query(
+    'UPDATE importado SET cantidad = $1 WHERE id = $2',
+    [cantidad, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`importado modificado ID: ${id}`)
+    }
+  )
+}
+//ELIMINA 1 importado con ID
+const elimImportado = (request, response) => {
+const id = parseInt(request.params.id)
+
+  pool.query(' DELETE FROM importado WHERE id_importacion = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Importe eliminado ID: ${id}`)
+  })
+}
 module.exports={
     getProductos,
     geProductoId,
     createProducto,
+    updateProducto,
+    elimProducto,
+
+    getPaises,
+    getPaisId,
+    createPais,
+    updatePais,
+    elimPais,
+
+    getImport,
+    getImportId,
+    createImportado,
+    updateImportado,
+    elimImportado,
 
 }
